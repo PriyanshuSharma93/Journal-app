f5a9b4057ce6b483405caf5a7b67eec24b3df168
Priyanshu Sharma <sharmapriyanshu8488@gmail.com>
2026-07-08 16:09:25 -0500

    Add Spring Security configuration and refactor user management

--- src/main/java/net/engineeringdigest/JournalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/JournalApp/controller/UserController.java
@@ -1,50 +1,47 @@
 package net.engineeringdigest.JournalApp.controller;
 
 
-import net.engineeringdigest.JournalApp.entity.JournalEntry;
 import net.engineeringdigest.JournalApp.entity.User;
-import net.engineeringdigest.JournalApp.service.JournalEntryService;
+import net.engineeringdigest.JournalApp.repository.UserRepository;
 import net.engineeringdigest.JournalApp.service.UserService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+
     @Autowired
     private UserService userService;
 
-   @GetMapping
-    public List<User> getAllUsers(){
-       return userService.getAll();
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb=userService.findByUserName(userName);
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveNewUser(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping
+    public ResponseEntity<?> deleteUserById(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 
-    @PostMapping
-    public void createUser(@RequestBody  User user){
-       userService.saveEntry(user);
-    }
-
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user,@PathVariable String userName){
-       User userInDb=userService.findByUserName(userName);
-       if(userInDb!=null){
-           userInDb.setUserName(user.getUserName());
-           userInDb.setPassword(user.getPassword());
-           userService.saveEntry(userInDb);
-       }
-       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-
-
-    }
+}
 
---
+++ src/main/java/net/engineeringdigest/JournalApp/service/UserDetailsServiceimpl.java
@@ -0,0 +1,32 @@
+package net.engineeringdigest.JournalApp.service;
+
+import net.engineeringdigest.JournalApp.entity.User;
+import net.engineeringdigest.JournalApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceimpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user=userRepository.findByUserName(username);
+        if(user!=null){
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUserName())
+                    .password(user.getPassword())
+                    .roles(user.getRoles().toArray(new String[0]))
+                    .build();
+        }
+        throw new UsernameNotFoundException("user not found with username:"+ username);
+
+    }
+
+}
