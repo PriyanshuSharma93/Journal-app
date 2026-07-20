422cef80b1092f87f3ff6d0bbdcd54ba56da8201
Priyanshu Sharma <sharmapriyanshu8488@gmail.com>
2026-07-20 09:15:26 -0500

    How to consume External POSTAPIs Effectively

--- src/main/java/net/engineeringdigest/JournalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/JournalApp/service/WeatherService.java
@@ -2,8 +2,11 @@ package net.engineeringdigest.JournalApp.service;
 
 import net.engineeringdigest.JournalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,6 +23,20 @@ public class WeatherService {
         ResponseEntity<WeatherResponse> response = resttemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
+
+
+//    public WeatherResponse getWeather(String city){
+//        String finalAPI = API.replace("CITY",city).replace ("API_KEY",apikey);
+//        HttpHeaders httpHeaders =new HttpHeaders();
+//        httpHeaders.set("key","value");
+//        User user = User.builder().username("priyanshu").password("priyanshu").build();
+//        HttpEntity<User> httpEntity=new HttpEntity<>(user);
+//        ResponseEntity<WeatherResponse> response = resttemplate.
+//        exchange(finalAPI, HttpMethod.POST,
+//        new httpEntity, WeatherResponse.class);
+//        WeatherResponse body = response.getBody();
+//        return body;
+//
 
     }
 }
